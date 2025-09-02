a7e3f610d8c3e7ad57bcf2315336da5a46d63852
KD Singh <kdsingh@KDs-MacBook-Air.local>
2025-09-02 17:25:34 +0530

    complete all type asynchronous pattern

--- promises/src/index.ts
+++ promises/src/index.ts
@@ -46,33 +46,205 @@
 
 // console.log("first finished");
 
-import fs from "fs";
+//<---------------Sequential Execution---------------->
+// import fs from "fs";
+// import { promisify } from "util";
 
-const executeTasksInSequence = () => {
-  console.log("🚀 Starting the process...");
-  setTimeout(() => {
-    console.log("⏳ Waiting for the first timeout...");
-    setTimeout(() => {
-      console.log("⏳ Waiting a bit more...");
-      fs.writeFile("test.txt", "Test File...", (error) => {
-        if (error) {
-          console.error("❌ Error writing file:", error);
-        } else {
-          console.log("✅ test.txt has been created");
-          setTimeout(() => {
-            fs.unlink("test.txt", (error) => {
-              if (error) {
-                console.error("❌ Error removing file:", error);
-              } else {
-                console.log("🗑️ test.txt has been removed");
-                console.log("🏁 Sequential execution complete");
-              }
-            });
-          }, 3000);
-        }
-      });
-    }, 2000);
-  }, 1000);
+// var writeFile = promisify(fs.writeFile);
+// var unlinkFile = promisify(fs.unlink);
+// var readdir = promisify(fs.readdir);
+
+// var wait = (sec: number) =>
+//   new Promise((resolves) => {
+//     setTimeout(resolves, sec * 1000);
+//   });
+
+// async function test() {
+//   var files = await readdir(__dirname);
+//   console.log(`Reading of files in done at`, files);
+// }
+// test();
+
+// const executeTasksInSequence = async () => {
+//   try {
+//     console.log("🚀 Starting the process...");
+//     await wait(1);
+//     console.log("⏳ Waiting for the first timeout...");
+//     await wait(2);
+//     console.log("⏳ Waiting a bit more...");
+//     await writeFile("test.txt", "Test File...");
+//     console.log("✅ test.txt has been created");
+//     await wait(3);
+//     await unlinkFile("test.txt");
+//     console.log("🗑️ test.txt has been removed");
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+// executeTasksInSequence();
+
+// const executeTasksInSequence = () =>
+//   Promise.resolve()
+//     .then(() => console.log("🚀 Starting the process..."))
+//     .then(() => {
+//       wait(1);
+//     })
+//     .then(() => "⏳ Waiting for the first timeout...")
+//     .then(console.log)
+//     .then(() => wait(2))
+//     .then(() => "⏳ Waiting a bit more...")
+//     .then(console.log)
+//     .then(() => writeFile("test.txt", "Test File..."))
+//     .then(() => "✅ test.txt has been created")
+//     .then(console.log)
+//     .then(() => wait(3))
+//     .then(() => unlinkFile("test.txt"))
+//     .then(() => "🗑️ test.txt has been removed")
+//     .then(() => console.log)
+//     .catch((error) => console.error(error));
+
+// executeTasksInSequence();
+
+// const executeTasksInSequence = () => {
+//   console.log("🚀 Starting the process...");
+//   setTimeout(() => {
+//     console.log("⏳ Waiting for the first timeout...");
+//     setTimeout(() => {
+//       console.log("⏳ Waiting a bit more...");
+//       fs.writeFile("test.txt", "Test File...", (error) => {
+//         if (error) {
+//           console.error("❌ Error writing file:", error);
+//         } else {
+//           console.log("✅ test.txt has been created");
+//           setTimeout(() => {
+//             fs.unlink("test.txt", (error) => {
+//               if (error) {
+//                 console.error("❌ Error removing file:", error);
+//               } else {
+//                 console.log("🗑️ test.txt has been removed");
+//                 console.log("🏁 Sequential execution complete");
+//               }
+//             });
+//           }, 3000);
+//         }
+//       });
+//     }, 2000);
+//   }, 1000);
+// };
+
+// executeTasksInSequence();
+
+//<----------- Parallel execution-------->
+
+import fs from "fs";
+import { promisify } from "util";
+import path from "path";
+import logUpdate from "log-update";
+const srcPath = path.join(process.cwd(), "src");
+const distPath = path.join(process.cwd(), "dist");
+
+var writeFile = promisify(fs.writeFile);
+var unlinkFile = promisify(fs.unlink);
+var readdir = promisify(fs.readdir);
+
+var wait = (sec: number) =>
+  new Promise((resolves) => {
+    setTimeout(resolves, sec * 1000);
+  });
+
+// console.log(process.cwd(), __dirname);
+
+// Promise.all([
+//   writeFile(path.join(srcPath, "test.md"), "This is test data"),
+//   writeFile(path.join(srcPath, "test.txt"), "This is test data"),
+//   writeFile(path.join(srcPath, "test.json"), "This is test data"),
+//   writeFile(path.join(srcPath, "test.docx"), "This is test data"),
+//   writeFile(path.join(distPath, "test.md"), "This is test data"),
+//   writeFile(path.join(distPath, "test.txt"), "This is test data"),
+//   writeFile(path.join(distPath, "test.json"), "This is test data"),
+//   writeFile(path.join(distPath, "test.docx"), "This is test data"),
+// ])
+//   .then(() => readdir(__dirname))
+//   .then(console.log);
+
+// Promise.all([wait(9), wait(8), wait(7), wait(2)])
+//   .then(() => readdir(__dirname))
+//   .then(console.log);
+
+// Promise.race([wait(9), wait(8), wait(7), wait(2)])
+//   .then(() => readdir(__dirname))
+//   .then(console.log);
+
+//<------------------------Concurrent Operations---------------->
+
+const toSandClock = () => "⌛";
+
+var wait = (sec: number) => {
+  return new Promise((resolves: any) => {
+    setTimeout(resolves, sec * 1000);
+  });
 };
 
-executeTasksInSequence();
+var tasks: any[] = [
+  wait(6),
+  wait(7),
+  wait(9),
+  wait(8),
+  wait(3),
+  wait(5),
+  wait(2),
+  wait(3),
+  wait(6),
+  wait(3),
+];
+
+class PromiseQueue {
+  concurrent: number;
+  total: number;
+  todo: any[];
+  running: any[];
+  done: any[];
+  constructor(promises: any[] = [], concurrentCount: number = 2) {
+    this.concurrent = concurrentCount;
+    this.total = promises.length;
+    this.todo = promises;
+    this.running = [];
+    this.done = [];
+  }
+  get runNewTask() {
+    return this.running.length < this.concurrent && this.todo.length;
+  }
+
+  logTask() {
+    const { todo, running, done } = this;
+    logUpdate(`
+      todo:[${todo.map(toSandClock)}],
+      running:[${running.map(toSandClock)}],
+      done:[${done.map(toSandClock)}],
+      `);
+  }
+
+  run() {
+    while (this.runNewTask) {
+      const promise = this.todo.shift();
+      this.running.push(promise);
+      this.logTask();
+
+      promise
+        .then(() => {
+          this.done.push(promise);
+        })
+        .catch((error: any) => {
+          console.log(error);
+        })
+        .finally(() => {
+          this.running = this.running.filter((p) => p != promise);
+           this.logTask();
+          this.run();
+        });
+    }
+  }
+}
+
+var tasksQueue = new PromiseQueue(tasks, 2);
+tasksQueue.run();
