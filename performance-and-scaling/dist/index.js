7b4e9c9c19974d2f11056fa3d2d0c23b8c09aa7c
KD Singh <rajputkdsingh999@gmail.com>
2025-09-17 22:06:28 +0530

    process pool learning completed

--- performance-and-scaling/dist/index.js
+++ performance-and-scaling/dist/index.js
@@ -1,9 +1,32 @@
 "use strict";
+// import { fork } from "child_process";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const child_process_1 = require("child_process");
-const process = [
-    (0, child_process_1.fork)("./dist/app.js", ["3001"]),
-    (0, child_process_1.fork)("./dist/app.js", ["3002"]),
-    (0, child_process_1.fork)("./dist/app.js", ["3003"]),
-];
-console.log(`Forked ${process.length} processes`);
+// const process = [
+//   fork("./dist/app.js", ["3001"]),
+//   fork("./dist/app.js", ["3002"]),
+//   fork("./dist/app.js", ["3003"]),
+// ];
+// console.log(`Forked ${process.length} processes`);
+const os_1 = __importDefault(require("os"));
+const cluster_1 = __importDefault(require("cluster"));
+const http_1 = __importDefault(require("http"));
+const cpuCount = os_1.default.cpus().length;
+// console.log("cpus===>", cpuCount.length);
+if (cluster_1.default.isMaster) {
+    console.log("Inside a master process", process.pid);
+    for (let i = 0; i < cpuCount; i++) {
+        cluster_1.default.fork();
+    }
+}
+else {
+    http_1.default
+        .createServer((req, res) => {
+        const msg = `This is a worker process with pidid:${process.pid}`;
+        console.log(msg);
+        res.end(msg);
+    })
+        .listen(3000);
+}
--- performance-and-scaling/src/index.ts
+++ performance-and-scaling/src/index.ts
@@ -1,9 +1,34 @@
-import { fork } from "child_process";
+// import { fork } from "child_process";
 
-const process = [
-  fork("./dist/app.js", ["3001"]),
-  fork("./dist/app.js", ["3002"]),
-  fork("./dist/app.js", ["3003"]),
-];
+// const process = [
+//   fork("./dist/app.js", ["3001"]),
+//   fork("./dist/app.js", ["3002"]),
+//   fork("./dist/app.js", ["3003"]),
+// ];
 
-console.log(`Forked ${process.length} processes`);
+// console.log(`Forked ${process.length} processes`);
+//npm i loadtest -g
+
+import os from "os";
+import cluster from "cluster";
+import http from "http";
+
+const cpuCount: number = os.cpus().length;
+// console.log("cpus===>", cpuCount.length);
+
+if (cluster.isMaster) {
+  console.log("Inside a master process", process.pid);
+  for (let i = 0; i < cpuCount; i++) {
+    cluster.fork();
+  }
+    
+} else {
+  console.log(`started a worker process at ${process.pid}.`);
+  http
+    .createServer((req, res) => {
+      const msg = `This is a worker process with pidid:${process.pid}`;
+      console.log(msg);
+      res.end(msg);
+    })
+    .listen(3000);
+}
